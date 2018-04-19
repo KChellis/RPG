54c2c749009eb67351897fab81378bb99c2e4827
Kristen Chellis and Susan Southall <susan@>
2018-04-19 14:17:40 -0700

    add enemy addValues function that passes test

--- src/enemy.js
+++ src/enemy.js
@@ -1,45 +1,34 @@
+import {Armor} from "./../src/armor.js";
+import {Weapon} from "./../src/weapon.js";
+
 class Enemy{
-  constructor(name, health, power, magic, defenseP, defenseM, agility) {
+  constructor(name) {
     this.name = name;
-    this.health = health;
-    this.power = power;
-    this.magic = magic;
-    this.defenseP = defenseP;
-    this.defenseM = defenseM;
-    this.agility = agility;
+    this.health;
+    this.power;
+    this.magic;
+    this.defenseP;
+    this.defenseM;
+    this.agility;
     this.expValue;
     this.weapon;
     this.armor;
     this.money;
   }
 
-  dealDamage(type) {
-    if(type === "power") {
-      let attack = this.power + this.weapon.power;
-      return [attack, type];
-    }else if(type === "magic") {
-      let attack = this.power + this.weapon.power;
-      return [attack, type];
+  addValues() {
+    if(this.name === "Goblin") {
+      this.health = 20;
+      this.power = 5;
+      this.magic = 3;
+      this.defenseP = 7;
+      this.defenseM = 6;
+      this.agility = 7;
+      this.expValue = 100;
+      this.weapon = new Weapon("sword", "power", 3);
+      this.armor = new Armor("leather","magic", 4);
+      this.money = 50;
     }
-    this.weapon.durability -= Math.floor(attack/10);
-  }
-
-  defend(attack) {
-    let hit;
-    if (attack[1] === "power" && this.armor.type === "power") {
-      hit = attack[0] - this.defenseP - this.armor.power;
-    }else if (attack[1] === "power") {
-      hit = attack[0] - this.defenseP;
-    }else if (attack[1] === "magic" && this.armor.type === "magic") {
-      hit = attack[0] - this.defenseM - this.armor.power;
-    }else if (attack[1] === "magic") {
-      hit = attack[0] - this.defenseM
-    }
-    if (hit <= 0) {
-      hit = 1;
-    }
-    this.weapon.durability -= Math.floor(attack[0]/10);
-    return hit;
   }
 }
 
