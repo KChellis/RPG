0a2d5238b54ded1eac2d9260e8971621292e2335
Kristen Chellis and Susan Southall <susan@>
2018-04-19 10:28:32 -0700

    add addValues function that passes test

--- src/skill.js
+++ src/skill.js
@@ -4,7 +4,61 @@ class Skill {
     this.power;
     this.type;
     this.duration;
-    this.turncount;
+    this.turncount = 0;
     this.active = true;
   }
+
+  addValues() {
+    let name = this.name;
+    if (name === "heal") {
+      this.power = 10;
+      this.type = "heal";
+      this.duration = "once";
+    }else if (name === "fire ball") {
+      this.power = 5;
+      this.type = "attackM";
+      this.duration = "once";
+    }else if (name === "pickpocket") {
+      this.power = 10;
+      this.type = "steal";
+      this.duration = "once";
+    }else if (name === "charge") {
+      this.power = 5;
+      this.type = "attackP";
+      this.duration = "once";
+    }else if (name === "counter attack") {
+      this.power = 5;
+      this.type = "counter";
+      this.duration = 3;
+    }else if (name === "cleave") {
+      this.power = 10;
+      this.type = "attackP";
+      this.duration = "once";
+    }else if (name === "backstab") {
+      this.power = 10;
+      this.type = "attackP";
+      this.duration = "once";
+    }else if (name === "blizzard") {
+      this.power = 10;
+      this.type = "attackM";
+      this.duration = "once";
+    }else if (name === "lightning") {
+      this.power = 15;
+      this.type = "attackM";
+      this.duration = "once";
+    }else if (name === "double slash") {
+      this.power = 15;
+      this.type = "attackP";
+      this.duration = "once";
+    }else if (name === "heal all") {
+      this.power = 30;
+      this.type = "attackP";
+      this.duration = "once";
+    }else if (name === "stoneskin") {
+      this.power = 10;
+      this.type = "buff";
+      this.duration = 3;
+    }
+  }
 }
+export {Skill};
